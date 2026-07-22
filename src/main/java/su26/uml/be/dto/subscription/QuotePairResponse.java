5b9dd7a961fd25e8eea4376625665332e6fe590a
TUNGUYEN\tungtung801 <duytung112005@gmail.com>
2026-07-22 18:38:26 +0700

    [fix]: Direct paymentKK

---
+++ src/main/java/su26/uml/be/dto/subscription/QuotePairResponse.java
@@ -0,0 +1,24 @@
+package su26.uml.be.dto.subscription;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Schema(name = "QuotePairResponse")
+public class QuotePairResponse {
+
+    @Schema(description = "Báo giá nâng cấp tiết kiệm (prorated)")
+    UpgradeQuoteResponse prorated;
+
+    @Schema(description = "Báo giá nâng cấp thẳng (direct)")
+    UpgradeQuoteResponse direct;
+}
